4590216bbf0b21f42858bd1dd428b59fa4b5e0be
mlicp1994 <mlicp1994@gmail.com>
2021-01-27 12:05:31 +0900

    spring boot study

--- board/src/main/java/com/june/board/repository/search/SearchBoardRepositoryImpl.java
+++ board/src/main/java/com/june/board/repository/search/SearchBoardRepositoryImpl.java
@@ -4,8 +4,13 @@ import com.june.board.entity.Board;
 import com.june.board.entity.QBoard;
 import com.june.board.entity.QMember;
 import com.june.board.entity.QReply;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
@@ -28,15 +33,70 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
 
         JPQLQuery<Board> jpqlQuery = from(board);
         jpqlQuery.leftJoin(member).on(board.writer.eq(member));
-        jpqlQuery.leftJoin(reply).on(reply.board.eq(board))
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
 
-        jpqlQuery.select(board).where(board.bno.eq(1L));
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+        tuple.groupBy(board);
 
         log.info("-------------------------");
-        log.info(jpqlQuery);
+        log.info(tuple);
         log.info("-------------------------");
 
-        List<Board> result = jpqlQuery.fetch();
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
+        return null;
+    }
+
+    @Override
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable){
+
+        log.info("SearchPage....................");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        BooleanExpression expression = board.bno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if(type != null){
+            String[] typeArr = type.split("");
+
+            BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+            for(String t:typeArr){
+                switch(t){
+                    case "t" :
+                        conditionBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "w":
+                        conditionBuilder.or(member.email.contains(keyword));
+                        break;
+                    case "c":
+                        conditionBuilder.or(board.content.contains(keyword));
+                        break;
+                }
+            }
+            booleanBuilder.and(conditionBuilder);
+        }
+
+        tuple.where(booleanBuilder);
+
+        tuple.groupBy(board);
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
 
         return null;
     }
