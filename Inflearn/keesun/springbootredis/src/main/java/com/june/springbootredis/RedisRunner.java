745fb5f814ee610eb50847f68524d55962ccb667
mlicp1994 <mlicp1994@gmail.com>
2021-04-16 15:53:30 +0900

    Springboot Study : connect NoSql DB

---
+++ Inflearn/keesun/springbootredis/src/main/java/com/june/springbootredis/RedisRunner.java
@@ -0,0 +1,40 @@
+package com.june.springbootredis;
+
+import com.june.springbootredis.account.Account;
+import com.june.springbootredis.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class RedisRunner implements ApplicationRunner {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set("hyeonjung", "developer");
+        values.set("spring boot", "2.0");
+        values.set("hello", "world");
+
+        Account account = new Account();
+        account.setEmail("hj.lee@wafour.com");
+        account.setUsername("hyeonjung");
+
+        accountRepository.save(account);
+
+        Optional<Account> byId = accountRepository.findById(account.getId());
+        System.out.println(byId.get().getUsername());
+        System.out.println(byId.get().getEmail());
+    }
+}
